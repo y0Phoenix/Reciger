5cbd091ab92acdd28d5baf791016af268b7fd6af
Aaron <aarongraybill3@gmail.com>
2022-02-28 15:20:20 -0600

    added optional prefs for new ingredient

--- src/layouts/models/IngredientM.js
+++ src/layouts/models/IngredientM.js
@@ -49,7 +49,7 @@ const IngredientM = ({showModal, setShowModal, postIngredient, getIngredients, l
 		}
 	  }
 	const [formData, setFormData] = useState(data);
-
+	const [showPrefs, setShowPrefs] = useState(false);
 	const {
 		name,
 		categories,
@@ -113,7 +113,7 @@ const IngredientM = ({showModal, setShowModal, postIngredient, getIngredients, l
 						<div className='new-ingredient-h'><h1>New Ingredient <i className='fa-solid fa-carrot'></i></h1></div>
 						<div className='new-ingredient-form'>
 							<h2 style={{"paddingLeft": "40px"}}>Form</h2>
-							<form onSubmit={e => onSubmit(e)}>
+							<form onSubmit={e => onSubmit(e)} autoComplete='off'>
 								<div className='new-ingredient-form-name'>
 									<input className='new-ingredient-form-input' type='text' value={name} name='name' onChange={e => onChange(e)} placeholder='name'></input>
 								</div>
@@ -123,15 +123,24 @@ const IngredientM = ({showModal, setShowModal, postIngredient, getIngredients, l
 								<div className='new-ingredient-form-price'>
 									<input className='new-ingredient-form-input' type='text' value={price} name='price' onChange={e => onChange(e)} placeholder='price'></input>
 								</div>
-								<div className='new-ingredient-form-volume'>
-									<input className='new-ingredient-form-input' type='text' value={volume} name='volume' onChange={e => onChange(e)} placeholder='volume measurement'></input>
-								</div>
-								<div className='new-ingredient-form-weight'>
-									<input className='new-ingredient-form-input' type='text' value={weight} name='weight' onChange={e => onChange(e)} placeholder='weight measurement'></input>
-								</div>
 								<div>
 									<input className='new-ingredient-form-input' type='text' value={prefered} name='prefered' onChange={e => onChange(e)} placeholder='prefered measurement'></input>
 								</div>
+								<input type='checkbox' value={showPrefs} onChange={e => setShowPrefs(e.target.checked)} className='new-ingredient-checkbox'></input>
+								<span className='new-ingredient-p'>Set Prefered Weight and Volume Specifically<br></br>
+								(defaults to standard oz, floz)</span>
+								{showPrefs &&
+									<>
+										<div className='new-ingredient-form-volume'>
+											<input className='new-ingredient-form-input' type='text' value={volume} name='volume' onChange={e => onChange(e)} placeholder='volume measurement'></input>
+										</div>
+										<div className='new-ingredient-form-weight'>
+											<input className='new-ingredient-form-input' type='text' value={weight} name='weight' onChange={e => onChange(e)} placeholder='weight measurement'></input>
+										</div>
+									</>
+								}
+								<br></br>
+								<br></br>
 								<div className='new-ingredient-form-nonut'>
 									<input className='new-ingredient-checkbox' type='checkbox' value={noNut} name='noNut' onChange={e => onChange(e)}></input>
 									<span className='new-ingredient-p'>No Nutritional Data</span>
