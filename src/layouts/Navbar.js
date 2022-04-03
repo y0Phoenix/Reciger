9553b052ae53ca4ca4e99bbaa5d3a2a23c90094a
Aaron <aarongraybill3@gmail.com>
2022-04-03 13:36:25 -0500

    init mobile layout for navbar and dashboard

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
@@ -8,37 +8,43 @@ const animate = {
 };
 
 const Navbar = ({isAuthenticated, logout, setNavigate}) => {
+  const [gap, setGap] = useState(window.innerWidth > 425 ? '35px' : '0');
+  window.addEventListener('resize' , (e) => e.target.innerWidth > 425 ? setGap('35px') : setGap('0'));
   return (
     <Fragment>
       {isAuthenticated ? 
       <div className='navbar-container'>
-        <div className='nav-item nav-1'>
-          <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/dashboard')}>
-           <i className="fa fa-folder-open"></i> Reciger 
-          </motion.button>
+        <div className='nav-2' style={{gap: gap}}>
+          <div>
+            <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/dashboard')}>
+            <i className="fa fa-folder-open"></i> Reciger 
+            </motion.button>
+          </div>
+          <div>
+            <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/recipes/1')}>
+            <i className='fa-solid fa-book'></i> Recipes 
+            </motion.button>
+          </div>
+          <div>
+            <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/ingredients/1')}>
+            <i className='fa-solid fa-carrot'></i> Ingredients 
+            </motion.button>
+          </div>
         </div>
-        <div className='nav-item nav-2'>
-          <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/recipes/1')}>
-           <i className='fa-solid fa-book'></i> Recipes 
-          </motion.button>
-        </div>
-        <div className='nav-item nav-3'>
-          <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/ingredients/1')}>
-           <i className='fa-solid fa-carrot'></i> Ingredients 
-          </motion.button>
-        </div>
-        <div className='nav-item nav-4'>
-          <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/account/personal')}>
-           <i className='fa-solid fa-user'></i> Account 
-          </motion.button>
-        </div>
-        <div className='nav-item nav-5'>
-          <motion.button whileHover={animate} className='nav-btn' onClick={e => {
-              logout(e);
-              setNavigate('/');
-            }}>
-           <i className="fa-solid fa-right-from-bracket"></i> Logout 
-          </motion.button>
+        <div className='nav-2' style={{gap: gap}}>
+          <div>
+            <motion.button whileHover={animate} className='nav-btn' onClick={() => setNavigate('/account/personal')}>
+            <i className='fa-solid fa-user'></i> Account 
+            </motion.button>
+          </div>
+          <div>
+            <motion.button whileHover={animate} className='nav-btn' onClick={e => {
+                logout(e);
+                setNavigate('/');
+              }}>
+            <i className="fa-solid fa-right-from-bracket"></i> Logout 
+            </motion.button>
+          </div>
         </div>
       </div> : 
       <div className='navbar-container'>
