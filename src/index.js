930733746218e08eacbeae6975dc54a05e5eb627
Aaron <aarongraybill3@gmail.com>
2022-04-05 12:50:02 -0500

    init scss overhaul

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://reciger-back-end.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://reciger-back-end.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:5000';
+// axios.defaults.baseURL = 'https://192.168.1.109:5000';
 axios.defaults.headers.post["Content-Type"] = 'application/json';
 
 ReactDOM.render(
