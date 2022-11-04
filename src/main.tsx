9b155ac8d0dd0ff01472d38de2d0580eecb73823
Aaron <aarongraybill3@gmail.com>
2022-11-03 22:26:10 -0500

    changed axios baseURL

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import axios from 'axios';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// axios.defaults.baseURL = 'https://reciger-back-end.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://reciger-back-end.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:5000';
 // axios.defaults.baseURL = 'https://192.168.1.109:5000';
 axios.defaults.headers.post["Content-Type"] = 'application/json';
 
