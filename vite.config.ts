ace9f8e6a18a0a200bc42837a4149d0dadb251de
Aaron <aarongraybill3@gmail.com>
2025-02-20 22:51:33 -0600

    fix: outdir on build for server

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    // outDir: '/var/www/html',
+    outDir: '/var/www/html',
     emptyOutDir: true,
   },
 });
