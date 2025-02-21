7e2d5d91542e403be80e57d28e3e3008a89c4e03
Aaron <aarongraybill3@gmail.com>
2025-02-20 22:39:47 -0600

    fix: updated api baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/var/www/html',
-    emptyOutDir: true
-  }
-})
+    // outDir: '/var/www/html',
+    emptyOutDir: true,
+  },
+});
